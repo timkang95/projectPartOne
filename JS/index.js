6f7c26666458923559fcacf06cd05e5d3b2cf092
Timothy Kang <timothykang8@gmail.com>
2016-10-11 15:28:33 -0400

    as of now

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,7 @@ function home() {
   document.getElementById("homeCard").style.visibility = "visible";
   document.getElementById("workExp").style.visibility = "hidden";
   document.getElementById("contactInfo").style.visibility = "hidden";
+  document.getElementById("pageFooter").style.marginTop = "1900px";
   return false;
 }
 
@@ -9,6 +10,7 @@ function workExp() {
   document.getElementById("homeCard").style.visibility = "hidden";
   document.getElementById("workExp").style.visibility = "visible";
   document.getElementById("contactInfo").style.visibility = "hidden";
+  document.getElementById("pageFooter").style.marginTop = "500px";
   return false;
 }
 
@@ -16,6 +18,7 @@ function contact() {
   document.getElementById("homeCard").style.visibility = "hidden";
   document.getElementById("workExp").style.visibility = "hidden";
   document.getElementById("contactInfo").style.visibility = "visible";
+  document.getElementById("pageFooter").style.marginTop = "-710px";
   return false;
 }
 
