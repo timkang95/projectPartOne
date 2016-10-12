810d105e074a7fe420158ad7905516781d03a442
Timothy Kang <timothykang8@gmail.com>
2016-10-12 19:25:23 -0400

    perhaps final commit

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,14 @@
+$(document).ready(function() {
+    $('#content').fadeIn(1000);
+});
+
 function home() {
   document.getElementById("homeCard").style.visibility = "visible";
   document.getElementById("workExp").style.visibility = "hidden";
   document.getElementById("contactInfo").style.visibility = "hidden";
-  document.getElementById("pageFooter").style.marginTop = "1900px";
+  document.getElementById("pageFooter").style.marginTop = "1775px";
+  document.getElementById("navBar").style.backgroundColor = "steelblue";
+  document.getElementById("pageFooter").style.backgroundColor = "steelblue";
   return false;
 }
 
@@ -10,7 +16,9 @@ function workExp() {
   document.getElementById("homeCard").style.visibility = "hidden";
   document.getElementById("workExp").style.visibility = "visible";
   document.getElementById("contactInfo").style.visibility = "hidden";
-  document.getElementById("pageFooter").style.marginTop = "500px";
+  document.getElementById("pageFooter").style.marginTop = "550px";
+  document.getElementById("navBar").style.backgroundColor = "orange";
+    document.getElementById("pageFooter").style.backgroundColor = "orange";
   return false;
 }
 
@@ -18,7 +26,9 @@ function contact() {
   document.getElementById("homeCard").style.visibility = "hidden";
   document.getElementById("workExp").style.visibility = "hidden";
   document.getElementById("contactInfo").style.visibility = "visible";
-  document.getElementById("pageFooter").style.marginTop = "-710px";
+  document.getElementById("pageFooter").style.marginTop = "50px";
+  document.getElementById("navBar").style.backgroundColor = "purple";
+  document.getElementById("pageFooter").style.backgroundColor = "purple";
   return false;
 }
 
